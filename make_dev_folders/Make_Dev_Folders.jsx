4550cf843204f3ca9824fa5a3456c6baae58402e
william <will.dowling@boombah.com>
2018-03-14 08:44:48 -0500

    added ability to export all artboards into pre-named size folders, typically for non-custom spiritwear orders.

--- make_dev_folders/Make_Dev_Folders.jsx
+++ make_dev_folders/Make_Dev_Folders.jsx
@@ -1,80 +1,152 @@
 function makeDevFolders()
 {
-	var valid,code,sizes = [];
-	var possibleSizes = ["YXS", "YS", "YM", "YL", "YXL", "Y2XL", "XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"]
+	var valid = true,
+		dest,
+		exportPreference = false,
+		code, sizes = [];
+	var possibleSizes = ["YXXS","YXS", "YS", "YM", "YL", "YXL", "Y2XL", "XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"]
+
+	eval("#include \"/Volumes/Customization/Library/Scripts/Script Resources/Data/Utilities_Container.jsxbin\"");
+
+	if (app.documents.length)
+	{
+		var docRef = app.activeDocument;
+		var aB = docRef.artboards;
+		var layers = docRef.layers;
+	}
+	else
+	{
+		errorList.push("Please open a document and try again.");
+		valid = false;
+	}
 
 	function dialogWindow()
 	{
 		function makeBoxes()
 		{
-			for(var x=0;x<possibleSizes.length;x++)
+			for (var x = 0; x < possibleSizes.length; x++)
 			{
 				checkboxGroup.add("checkbox", undefined, possibleSizes[x])
 			}
 		}
 
 
+		/* beautify ignore:start */
 		var w = new Window("dialog", "Enter the info.");
 			var codeGroup = w.add("group");
 				var codeTxt = codeGroup.add("statictext", undefined, "Enter the Garment Code Info:");
 				var codeInput = codeGroup.add("edittext", undefined, "FD-000-0000");
-					codeInput.characters = 20;
+				codeInput.characters = 20;
 
 			var sizeGroup = w.add("group");
 				sizeGroup.orientation = "column";
-				var sizeTxt = sizeGroup.add("statictext", undefined, "Select the sizes you'll need:");
-				var checkboxGroup = sizeGroup.add("group");
-				makeBoxes();
+			var sizeTxt = sizeGroup.add("statictext", undefined, "Select the sizes you'll need:");
+			var checkboxGroup = sizeGroup.add("group");
+			makeBoxes();
 
-				var sizeInput = sizeGroup.add("edittext", undefined, "Enter any additional sizes you need, spearated by commas.");
-					sizeInput.characters = 20;
+			var sizeInput = sizeGroup.add("edittext", undefined, "Enter any additional sizes you need, spearated by commas.");
+				sizeInput.characters = 20;
 
 			var btnGroup = w.add("group");
-				var submit = btnGroup.add("button", undefined, "Submit");
-					submit.onClick = function()
-					{
-						code = codeInput.text;
-						for(var x=0;x<checkboxGroup.children.length;x++)
-						{
-							var thisBox = checkboxGroup.children[x];
-							if(thisBox.value)
-							{
-								sizes.push(thisBox.text);
-							}
-						}
-						if(sizeInput.text != "" && sizeInput.text.indexOf("Enter") == -1)
-						{
-							var otherSizes = sizeInput.text.split(",");
-							for(var x=0;x<otherSizes.length;x++)
-							{
-								sizes.push(otherSizes[x]);
-							}
-						}
-						valid = true;
-						w.close();
-					}
+				var foldersOnly = btnGroup.add("button", undefined, "Create Folders Only");
+				foldersOnly.onClick = function()
+				{
+					logSizes();
+					w.close();
+				}
+
+				var foldersAndExport = btnGroup.add("button", undefined, "Export Artboards to Size Folders");
+				foldersAndExport.onClick = function()
+				{
+					exportPreference = true;
+					logSizes();
+					w.close();
+				}
 
 				var cancel = btnGroup.add("button", undefined, "Cancel");
-					cancel.onClick = function()
-					{
-						valid = false;
-						w.close();
-					}
+				cancel.onClick = function()
+				{
+					valid = false;
+					w.close();
+				}
 
 		w.show();
+		/* beautify ignore:end */
+
+
+		function logSizes()
+		{
+			code = codeInput.text;
+			for (var x = 0; x < checkboxGroup.children.length; x++)
+			{
+				var thisBox = checkboxGroup.children[x];
+				if (thisBox.value)
+				{
+					sizes.push(thisBox.text);
+				}
+			}
+			if (sizeInput.text != "" && sizeInput.text.indexOf("Enter") == -1)
+			{
+				var otherSizes = sizeInput.text.split(",");
+				for (var x = 0; x < otherSizes.length; x++)
+				{
+					sizes.push(otherSizes[x]);
+				}
+			}
+		}
+	}
+
+
+	function exportSize(curSize,dest)
+	{
+		var curItem, pdfFile;
+		var pdfSaveOpts = new PDFSaveOptions();
+			pdfSaveOpts.preserveEditability = false;
+			pdfSaveOpts.viewAfterSaving = false;
+			pdfSaveOpts.compressArt = true;
+			pdfSaveOpts.optimization = true;
+			pdfSaveOpts.artboardRange = 1;
+
+		// var curSizeFolder = Folder("/Volumes/Macintosh HD" + dest.fsName + "/" + curSize);
+		// if(!curSizeFolder.exists)
+		// {
+		// 	curSizeFolder.create();
+		// }
+
+		for (var x = 0, len = aB.length; x < len; x++)
+		{
+			aB.setActiveArtboardIndex(x);
+			docRef.selectObjectsOnActiveArtboard();
+			curItem = docRef.selection[0];
+			if (curItem.name.indexOf(curSize) === 0)
+			{
+				pdfSaveOpts.artboardRange = (x + 1) + "";
+				pdfFile = File(dest + "/" + code + "-" + curItem.name + ".pdf");
+				docRef.saveAs(pdfFile,pdfSaveOpts);
+			}
+		}
 	}
 
 	function makeFolders()
 	{
 		var user = $.getenv("USER");
-		var dest = new Folder("/Volumes/Macintosh HD/Users/" + user + "/Desktop/" + code);
+		// var dest = new Folder("/Volumes/Macintosh HD/Users/" + user + "/Desktop/" + code);
 		// var dest = new Folder("~/Desktop/" + code);
-		dest.create();
+		dest = Folder("/Volumes/Macintosh HD" + desktopFolder.selectDlg("Please choose a save location.").fsName + "/" + code);
 
-		for(var x=0;x<sizes.length;x++)
+		if (!dest.exists)
+		{
+			dest.create();
+		}
+
+		for (var x = 0; x < sizes.length; x++)
 		{
 			var newDest = new Folder(dest.fsName + "/" + code + "-" + sizes[x]);
 			newDest.create();
+			if(exportPreference)
+			{
+				exportSize(sizes[x],newDest);
+			}
 		}
 	}
 
